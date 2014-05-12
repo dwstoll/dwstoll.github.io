f093cc503cb62ad51a9c8c860608a1daf9de8793
Daniel <info@dwstoll.de>
2014-05-12 10:07:51 +0200

    Test
    1234

---
+++ Codebeispiele/HauptController.js
@@ -0,0 +1,22 @@
+function AdressbuchController($scope) {	
+	$scope.benutzer = [
+	{"vorname": "Max", "name": "Meier"},
+	{"vorname": "Florian", "name": "Wiegand"},
+	{"vorname": "Kai", "name": "Lehner"},
+	{"vorname": "Markus", "name": "Siebert"},
+	{"vorname": "Fritz", "name": "Meier"},
+	{"vorname": "Lisa", "name": "Reichert"},
+	{"vorname": "Nadine", "name": "Herbold"},
+	{"vorname": "Anne", "name": "Reichert"},
+	{"vorname": "Alexander", "name": "Priebe"},
+	{"vorname": "Andreas", "name": "Fix"},
+	{"vorname": "Christian", "name": "Decker"},
+	{"vorname": "Florian", "name": "Lechner"},
+	{"vorname": "Leon", "name": "Saam"},
+	{"vorname": "Julian", "name": "Meier"},
+	{"vorname": "Julia", "name": "Siebig"},
+	{"vorname": "Miriam", "name": "Franzke"},
+	{"vorname": "Tanja", "name": "Marx"}
+	
+	];	
+}
