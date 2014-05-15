e17c4e4a3f2a2ed846cd87aac62ecef050994153
Daniel <info@dwstoll.de>
2014-05-15 11:26:36 +0200

    dd
    d

---
+++ HTML5-Praes/Deck/HauptController.js
@@ -0,0 +1,12 @@
+function AdressbuchController($scope) {	
+	$scope.benutzer = [
+	{"vorname": "Max", "name": "Meier"},
+	{"vorname": "Florian", "name": "Wiegand"},
+	{"vorname": "Kai", "name": "Lehner"},
+	{"vorname": "Markus", "name": "Siebert"},
+	{"vorname": "Fritz", "name": "Meier"},
+	{"vorname": "Lisa", "name": "Reichert"},
+	{"vorname": "Florian", "name": "Herbold"}
+	
+	];	
+}
