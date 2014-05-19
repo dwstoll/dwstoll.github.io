4921e11f970119505c1c5e0e234f6abda31aa43b
dwstoll <info@dwstoll.de>
2014-05-19 09:29:07 +0200

    1
    1

--- Codebeispiele/Adressbuch-ver1.js
+++ Codebeispiele/Adressbuch-ver1.js
@@ -1,4 +1,7 @@
+// Funktion ist Bestandteil eines Adressbuch-Objekts
+
 function sucheAdressbuch(benutzer) {	
+	// neues Datenbank-Objekt wird innerhalb der Funktion erzeugt
 	var db = new Database(...);
 	
 	db.suche(benutzer);
